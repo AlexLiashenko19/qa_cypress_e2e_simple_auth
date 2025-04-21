8411b61ac092daa1dfac5eccf405a1d2b1b5d9c1
Александр Ляшенко <sanko.lialko14@gmail.com>
2025-04-21 12:42:43 +0300

    d

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  baseUrl: 'https://the-internet.herokuapp.com/login',
   e2e: {
     setupNodeEvents(on, config) {
     }
--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -2,10 +2,43 @@
 
 describe('Sign In page', () => {
   beforeEach(() => {
-
+    cy.visit('/');
   });
 
-  it('', () => {
+  it('should log in successfully with correct credentials', () => {
+    cy.get('input=[name="username"]').type('tomsmith');
+    cy.get('input=[name="password"]').type('SuperSecretPassword');
+    cy.get('button=["submit"]').click();
 
+    cy.get('#flash')
+      .should('have.class', 'success')
+      .and('contain', 'You logged into a secure area!');
+  });
+
+  it('should fail to log in with incorrect credentials', () => {
+    cy.get('input=[name="username"]').type('tomsmith');
+    cy.get('input=[name="password"]').type('SuperSecretPassword');
+    cy.get('button=["submit"]').click();
+
+    cy.get('#flash')
+      .should('be.visible')
+      .and('have.class', 'error')
+      .invoke('text')
+      .then((text) => {
+        const normalizedText = text.replace(/\s+/g, ' ').trim();
+        expect(normalizedText).to.include('Your password is invalid!');
+      });
+  });
+
+  it('should log out successfully after logging in', () => {
+    cy.get('input=[name="username"]').type('tomsmith');
+    cy.get('input=[name="password"]').type('SuperSecretPassword');
+    cy.get('button=["submit"]').click();
+
+    cy.get('a[href="/logout"]').click();
+
+    cy.get('#flash')
+      .should('have.class', 'success')
+      .and('contain', 'You logged out of the secure area!');
   });
 });
